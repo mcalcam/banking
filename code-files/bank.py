fdb7de6dc0b5a07dffd6052fd74566813d803b86
Michael Calcamuggio <mcalcamuggio2@gmail.com>
2024-10-15 20:21:23 -0600

    added more menu options

--- code-files/bank.py
+++ code-files/bank.py
@@ -1,24 +1,31 @@
-import datetime
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
 
 ACCOUNTS = []
 LOANS = []
 CUSTOMERS = []
-CURREN_DATE = datetime.date.today()
+CURRENT_DATE = date.today()
 
 class Loan:
     def __init__(self, principal):
         self.number = len(LOANS) + 1
         self.principal = principal
+        self.open_date = CURRENT_DATE
         # self.rate
         # self.accrued_interest
         # self.balance
 
         LOANS.append(self)
+    
+    def calculate_interest(self):
+        pass
 
 class Account:
     def __init__(self):
         self.number = len(ACCOUNTS) + 1
         self.balance = 0.00
+        self.open_date = CURRENT_DATE
         ACCOUNTS.append(self)
 
 class Customer: 
@@ -43,11 +50,30 @@ class Customer:
     def withdraw(self):
         pass
 
+def advance_date():
+    global CURRENT_DATE
+    CURRENT_DATE = CURRENT_DATE + relativedelta(months=1)
+    print('Date advanced')
+
+def print_options():
+    print(f'Today: {CURRENT_DATE.strftime("%A %d. %B %Y")}')
+    print('1: Advance to the next month')
+    print()
+
+def execute_option(option):
+    if option == 1:
+        advance_date()
+
+
 def main():
     user_input = ''
-    while user_input != 'q':
-        
+    while True:
+        print_options()
         user_input = input('Select an option: ')
+        if user_input == 'q':
+            return
+        else:
+            execute_option(int(user_input))
 
 if __name__=="__main__":
     main()
